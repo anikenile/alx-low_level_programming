db1a33f37bde04e876dbd2c5ed370ea7fc2156e9
Kenile <anikenile@gmail.com>
2023-04-21 13:59:15 -0700

    task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - code enters here
+ * program to write alphabets in reverse
+ * Return: Always 0 as result
+ */
+int main(void)
+{
+	int ch;
+
+	for (ch = 'z'; ch >= 'a'; ch--)
+		putchar(ch);
+	return (0);
+}
