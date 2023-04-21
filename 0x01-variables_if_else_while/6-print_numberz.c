c45f67ae4166a1afbb279dda46babcc1738a67c5
Kenile <anikenile@gmail.com>
2023-04-21 12:35:35 -0700

    task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry of code
+ * code to print numbers from 0 to 9
+ * Return: Always 0 as result
+ */
+int main(void)
+{
+        int num;
+
+        for (num = 0; num < 10; num++)
+        {
+                printf("%d", num);
+        }
+        putchar('\n');
+        return (0);
+}
