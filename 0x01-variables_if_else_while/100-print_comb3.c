9bb87d058094d4d33d374789f8b14d63506b9f2b
Kenile <anikenile@gmail.com>
2023-04-21 14:50:40 -0700

    hmmm

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - Entry to code
+ * code to print all possible combinations of two digits
+ * Return: Always 0 as output
+ */
+int main(void)
+{
+	int x, y;
+
+	for (x = 48; x <= 56; x++)
+	{
+		for (y = 49; y <= 57; y++)
+		{
+			if (y > x)
+			{
+				putchar(x);
+				putchar(y);
+				if (x !=56 || y != 56)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
