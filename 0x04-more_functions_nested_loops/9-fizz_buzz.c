843b69009f54cd4fbceececfa4ea9dc6fa3028d7
Kenile <anikenile@gmail.com>
2023-04-27 14:39:26 -0700

    fizz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -16,21 +16,21 @@ int main(void)
 		}
 		else if (((i % 5) == 0) && ((i % 3) != 0))
 		{
-			printf("Buzz ");
+			printf(" Buzz");
 		}
 		else if ((i % 15) == 0)
 		{
-			printf("FizzBuzz ");
+			printf(" FizzBuzz");
 		}
-		else if (i == 100)
+		else if (i == 1)
 		{
 			printf("%d", i);
 		}
 		else
 		{
-			printf("%d ", i);
+			printf( "%d ", i);
 		}
 	}
-	putchar('\n');
+	printf("\n");
 	return (0);
 }
