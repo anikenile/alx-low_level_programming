42099a4947e7311d0b0ca7be4e1401a0299fcb71
Kenile <anikenile@gmail.com>
2023-04-27 14:43:21 -0700

    fizzbuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - Fizz-Buzz test
+ * main - FizzBuzz test
  * Return: 0
  */
 int main(void)
@@ -28,7 +28,7 @@ int main(void)
 		}
 		else
 		{
-			printf( "%d ", i);
+			printf("%d ", i);
 		}
 	}
 	printf("\n");
