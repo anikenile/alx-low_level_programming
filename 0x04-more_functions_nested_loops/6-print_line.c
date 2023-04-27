2494297cb7145132616d98fca5a1a5ca3fa43523
Kenile <anikenile@gmail.com>
2023-04-27 10:09:15 -0700

    line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_line - draws a straight line in the terminal
+ * @n: number of times to print _
+ */
+void print_line(int n)
+{
+	if (n <= 0)
+		_putchar('\n');
+	else
+		_putchar(n);
+}
