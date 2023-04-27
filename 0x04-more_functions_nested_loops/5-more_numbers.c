73bba9f715d74f5379a6a69f14ecb36a35b44f8d
Kenile <anikenile@gmail.com>
2023-04-27 09:19:23 -0700

    ten

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints 10 times the number, from 0 to 14
+ * Return: 0
+ */
+void more_numbers(void)
+{
+	int i = 1, j;
+
+	while (i < 11)
+	{
+		j = 0;
+
+		while (j < 15)
+
+		_putchar(j);
+		j++;
+	}
+	_putchar('\n');
+}
