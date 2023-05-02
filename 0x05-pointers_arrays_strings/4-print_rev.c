247fef086fd4d09e20677de9e660a97be70f6fb8
Kenile <anikenile@gmail.com>
2023-05-02 14:57:19 -0700

    rev

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,8 +8,11 @@
 void _puts(char *str)
 {
         int prnt;
+	int count = 0;
 
-        for (prnt = str[prnt]; str[prnt] != '\0'; prnt++)
+        for (prnt = 0; str[prnt] != '\0'; prnt++)
+		count++;
+	for (prnt = count; prnt >= 0; prnt--)
                 _putchar(str[prnt]);
         _putchar('\n');
 }
