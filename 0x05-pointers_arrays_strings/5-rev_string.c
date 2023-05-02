d7eac410030f01c2138da68128f8a7816096b577
Kenile <anikenile@gmail.com>
2023-05-02 16:21:00 -0700

    rev2

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * rev_string - a function that reverses a string
+ * @s: the string to reverse
+ * Return: 0
+ */
+void rev_string(char *s)
+{
+	int x;
+	int count = 0;
+	char rev = s[0];
+
+	if (s[count] != '\0')
+		count++;
+	for (x = 0; x < count; x++)
+	{
+		count--;
+		rev = s[x];
+		s[x] = s[count];
+		s[count] = rev;
+	}
+}
