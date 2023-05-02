42e1ddb7337282da09fa143884f904c8f2951ace
Kenile <anikenile@gmail.com>
2023-05-02 15:31:12 -0700

    rev

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -10,9 +10,16 @@ void print_rev(char *s)
 	int p;
 	int count = 0;
 
-	for (p = 0; s[p] != '\0'; p++)
+	while (*s != '\0')
+	{
 		count++;
-	for (p = count; p >= 0; p--)
-		_putchar(s[p]);
+		s++;
+	}
+	s--;
+	for (p = count; p > 0; p--)
+	{
+		_putchar(*s);
+		s--;
+	}
 	_putchar('\n');
 }
