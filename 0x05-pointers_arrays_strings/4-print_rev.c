bf6e53e4c1e203c76a08afea084dcc65dcdc6df5
Kenile <anikenile@gmail.com>
2023-05-02 15:01:53 -0700

    rev

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,18 +1,18 @@
 #include "main.h"
 
 /**
- * _puts - prints a string
- * @str: string to print
+ * print_rev - prints a string
+ * @s: string to print
  * Return: 0
  */
-void _puts(char *str)
+void print_rev(char *s)
 {
         int prnt;
 	int count = 0;
 
-        for (prnt = 0; str[prnt] != '\0'; prnt++)
+        for (prnt = 0; s[prnt] != '\0'; prnt++)
 		count++;
 	for (prnt = count; prnt >= 0; prnt--)
-                _putchar(str[prnt]);
+                _putchar(s[prnt]);
         _putchar('\n');
 }
