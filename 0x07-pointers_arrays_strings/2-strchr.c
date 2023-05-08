53e54a67b64e7daeb2a236eb2dd6257a2fc265db
Kenile <anikenile@gmail.com>
2023-05-08 10:10:38 -0700

    strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * *_strchr - locates a character in a string
+ * @s: string
+ * @c: character
+ * Return: pointer
+ */
+char *_strchr(char *s, char c)
+{
+	int x = 0;
+
+	for (; s[x] >= '\0'; x++)
+	{
+		if (s[x] == c)
+			return (&s[x]);
+	}
+	return (0);
+}
