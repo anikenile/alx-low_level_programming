fd39c54311b1bce227eb70b3e2da2ac97b41251a
Kenile <anikenile@gmail.com>
2023-05-08 12:01:30 -0700

    strstr

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * *_strstr - locates a substring
+ * @haystack: string
+ * @needle: substring
+ * Return: 0
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	for (; *haystack != '\0'; haystack++)
+	{
+		char *x = haystack;
+		char *y = needle;
+
+		while (*x == *y && *y != '\0')
+		{
+			x++;
+			y++;
+		}
+
+		if (*y == '\0')
+			return (haystack);
+	}
+
+	return (0);
+}
