b44f7dc7662dd4449b43149d749155e33a38f07e
Kenile <anikenile@gmail.com>
2023-05-12 19:17:37 -0700

    argc

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -12,18 +12,18 @@ int main(int argc, char *argv[])
 	int fig1, fig2;
 	int mult;
 
-	fig1 = atoi(argv[1]);
-	fig2 = atoi(argv[2]);
-
-	if (argc == 3)
+	if (argc < 3 || argc > 3)
 	{
+		printf("Error\n");
+		return (1);
+	}
+	else if (argc == 3)
+	{
+		fig1 = atoi(argv[1]);
+		fig2 = atoi(argv[2]);
+
 		mult = fig1 * fig2;
 		printf("%d\n", mult);
-	}
-	else if (argc < 3 || argc > 3)
-	{
-		printf("Error");
-		return (1);
 	}
 	return (0);
 }
