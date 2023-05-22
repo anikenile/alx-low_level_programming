03ed15caf23edec1462f6aeb843f31a7c3ace7c0
Kenile <anikenile@gmail.com>
2023-05-22 10:59:41 -0700

    dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,13 @@
 #ifndef HEADER_FILE
 #define HEADER_FILE
 
+/**
+ * struct dog - new type to create
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ */
+
 typedef struct dog
 {
 	char *name;
