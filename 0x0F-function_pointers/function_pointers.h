714d2baeb586a7039d717b4c6f3ce56c7c514b5f
Kenile <anikenile@gmail.com>
2023-05-24 10:02:25 -0700

    name

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,6 +1,9 @@
-#include <stddef.h>
 #ifndef HEADER_FILE
 #define HEADER_FILE
+
+#include <stddef.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
