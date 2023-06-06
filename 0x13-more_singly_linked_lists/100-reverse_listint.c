f5276a8e6271dbbaf1baab3b964d81d7ecce8082
Kenile <anikenile@gmail.com>
2023-06-06 16:01:34 -0700

    reveres

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -8,12 +8,12 @@
  * Return: a pointer to the first node
  */
 listint_t *reverse_listint(listint_t **head)
-{       
+{
 	listint_t *previousNode = NULL;
 	listint_t *nextNode = NULL;
 
 	while (*head != NULL)
-	{       
+	{
 		nextNode = (*head)->next;
 		(*head)->next = previousNode;
 		previousNode = *head;
@@ -21,4 +21,4 @@ listint_t *reverse_listint(listint_t **head)
 	}
 	*head = previousNode;
 	return (*head);
-}       
+}
