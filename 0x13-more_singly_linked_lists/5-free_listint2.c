3da85b1ad385517c02d4732559e5e0194f586692
Kenile <anikenile@gmail.com>
2023-06-06 00:53:28 -0700

    free2

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -8,12 +8,12 @@
  */
 void free_listint2(listint_t **head)
 {
-	listint_t *newHead = *head;
+	listint_t *newHead;
 
 	if (head == NULL)
 		return;
 
-	while (*head)
+	while (*head != NULL)
 	{
 		newHead = (*head)->next;
 		free(*head);
