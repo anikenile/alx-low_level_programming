815f51246b899d74122aaf206173f8446d33fcf9
Kenile <anikenile@gmail.com>
2023-06-06 02:06:58 -0700

    nth

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -13,7 +13,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	listint_t *newNode = head;
 	unsigned int i;
 
-	if (newNode == NULL)
+	if (!newNode)
 		return (NULL);
 
 	for (i = 0; i < index; i++)
