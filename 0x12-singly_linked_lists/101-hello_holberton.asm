34f5419fee9f2c9a36f7b4e7b303a7bedce1e475
Kenile <anikenile@gmail.com>
2023-06-07 00:28:32 -0700

    puts

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,16 +1,11 @@
-; prints Hello, Holberton in 64bit assembly language
-
-global    _start
+	global    main
+	extern	  puts
 
           section   .text
-_start:   mov       rax, 1                 
-          mov       rdi, 1                 
-          mov       rsi, msg            
-          mov       rdx, 13                 
-          syscall                         
-          mov       rax, 60                
-          xor       rdi, rdi                
-          syscall         
+main:
+          mov       rdi, resMsg            
+          call	    puts
+	  ret
 
           section   .data
-msg:	  db        "Hello, Holberton", 10
+resMsg:	  db        "Hello, Holberton", 0
