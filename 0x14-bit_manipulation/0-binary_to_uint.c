361f3400aa6332fbefc877cb86e931905cc60dd2
Kenile <anikenile@gmail.com>
2023-06-08 12:16:30 -0700

    binary

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -14,10 +14,11 @@ unsigned int binary_to_uint(const char *b)
 	if (b == NULL)
 		return (0);
 
+	if (b[i] < '0' || b[i] > '1')
+		return (0);
+
 	for (i = 0; i < b[i]; )
 	{
-		if (b[i] < '0' || b[i] > '1')
-			return (0);
 		ret = 2 * ret + (b[i] - '0');
 		i++;
 	}
