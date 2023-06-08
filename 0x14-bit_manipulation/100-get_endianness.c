0e8018b21837f98437aa5fdf38afe5825332d12b
Kenile <anikenile@gmail.com>
2023-06-08 15:41:08 -0700

    Endian

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 #include "main.h"
 
-/** get_endianness - check order in which a sequence of bytes is stored
+/**
+ * get_endianness - check order in which a sequence of bytes is stored
  * Return: Little or big Endian
  */
 int get_endianness(void)
 {
 	int a = 1;
-	char *c = (char *) & a;
+	char *c = (char *) &a;
+
 	return (*c);
 }
