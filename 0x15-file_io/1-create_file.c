64215f32ff315559ab03db919ad2e6659d54ac18
Kenile <anikenile@gmail.com>
2023-06-13 10:08:21 -0700

    create

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,19 +12,16 @@ int create_file(const char *filename, char *text_content)
 	int op, wr;
 	int len;
 
-	if (filename == NULL)
-		return (-1);
-
-	if (text_content != NULL)
+	for (len = 0; text_content[len] ; )
 	{
-		for (len = 0; text_content[len]; )
+		if (text_content != NULL)
 			len++;
 	}
 
 	op = open(filename, O_CREAT | O_RDWR | O_TRUNC, 5000);
 	wr = write(op, text_content, len);
 
-	if (op == -1 || wr == -1)
+	if (filename == NULL || op == -1 || wr == -1)
 		return (-1);
 
 	close(op);
