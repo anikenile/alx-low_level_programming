9ec6ced1be7c44ea58643b86a2ec7662c446ce2d
anikenile <anikenile@gmail.com>
2023-06-27 15:01:54 -0700

    Update

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -2,6 +2,16 @@
 #include "main.h"
 
 /**
+ * exitWithError - code to format exit messages
+ * @message: exit message to show
+ */
+void exitWithError(const char *message)
+{
+	dprintf(STDERR_FILENO, "Error: %s\n", message);
+	exit(1);
+}
+
+/**
  * main - a program that copies the content of a file to another file
  * @argc: number of arguments it takes
  * @argv: range of values
@@ -9,8 +19,9 @@
  */
 int main(int argc, char *argv[])
 {
-	int from, to, rd, op;
+	int from, to;
 	char *buf = malloc(sizeof(char) * 1024);
+	ssize_t rd, op;
 
 	if (argc != 3)
 	{
@@ -21,34 +32,46 @@ int main(int argc, char *argv[])
 	buf = argv[2];
 
 	from = open(argv[1], O_RDWR);
+	if (from == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+
+
 	rd = read(from, buf, 1024);
-	to = open(argv[2], O_CREAT | O_RDWR | 0664);
+	to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC | 0644);
+
+	if (to == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
 
 	while (rd > 0)
 	{
-		if (from == -1 || rd == -1)
-		{
-			dprintf(STDERR_FILENO,
-					"Error: Can't read from file %s\n", argv[1]);
-			free(buf);
-			exit(98);
-		}
 		op = write(to, buf, rd);
 
-		if (to == -1 || op == -1 || buf == NULL)
+		if (op == -1)
 		{
-			dprintf(STDERR_FILENO,
-				"Error: Can't write to %s\n", argv[2]);
-			free(buf);
+			dprintf(STDERR_FILENO, "Error:: Can't write to %s\n", argv[2]);
 			exit(99);
 		}
-
-		rd = read(from, buf, 1024);
-		to = open(argv[2], O_RDWR | O_APPEND);
-
-		free(buf);
-		close(from);
-		close (to);
 	}
-	return (1);
+	if (rd == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+        exit(98);
+	}
+	if (close(from) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", from);
+        exit(100);
+    }
+	if (close(to) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", to);
+        exit(100);
+    }
+	return (0);
 }
